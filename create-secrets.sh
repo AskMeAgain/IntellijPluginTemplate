d5f30bd70141da4dc9dfa1e13f8b9041c3b7053b
AskMeAgain <20632124+AskMeAgain@users.noreply.github.com>
2022-08-21 11:39:11 +0200

    Creating explanation

---
+++ create-secrets.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+mkdir -p secrets
+
+cd secrets
+
+openssl genpkey -aes-256-cbc -algorithm RSA -out PRIVATE_KEY.pem -pkeyopt rsa_keygen_bits:4096
+
+openssl req -key PRIVATE_KEY.pem -new -x509 -days 365 -out CERTIFICATE_CHAIN.crt
+
